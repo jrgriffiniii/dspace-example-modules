357cbcc4596c25c58f9e6fd3e4aacf39b3f27a9a
James R. Griffin III <jrgriffiniii@gmail.com>
2020-05-23 14:36:59 -0400

    Starting to fix the database (finally)

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ This is an experiment
 ***## Getting started
 
 ```
-docker run --name dspace-test-postgres -p 55432:5432 -e POSTGRES_USER=dspace -e POSTGRES_PASSWORD=dspace -d postgres
+docker run --name dspace-test-postgres -p 55432:5432 -e POSTGRES_USER=dspace -e POSTGRES_PASSWORD=dspace -d postgres:9.0
 ```
+
+An administrator group needs to be manually created
 
 ```
 mvn dependency:go-offline
--- src/test/java/edu/princeton/library/dspace/ExampleTaskTest.java
+++ src/test/java/edu/princeton/library/dspace/ExampleTaskTest.java
@@ -2,6 +2,7 @@ package edu.princeton.library.dspace;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import org.dspace.core.Context;
 import org.dspace.core.I18nUtil;
 import org.dspace.eperson.EPerson;
@@ -21,6 +22,27 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.TimeZone;
+
+import org.apache.log4j.Logger;
+import org.dspace.app.util.MockUtil;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Context;
+import org.dspace.core.I18nUtil;
+import org.dspace.discovery.MockIndexEventConsumer;
+
+import org.dspace.servicemanager.DSpaceKernelImpl;
+import org.dspace.servicemanager.DSpaceKernelInit;
+import org.dspace.storage.rdbms.MockDatabaseManager;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(EPerson.class)
 public class ExampleTaskTest {
@@ -28,6 +50,53 @@ public class ExampleTaskTest {
   private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
   private final PrintStream originalOut = System.out;
   private final PrintStream originalErr = System.err;
+
+  private static final Logger log = Logger.getLogger(ExampleTaskTest.class);
+  protected static Properties testProps;
+  protected Context context;
+  protected static DSpaceKernelImpl kernelImpl;
+
+  @BeforeClass
+  public static void initOnce()
+  {
+    try
+    {
+      //set a standard time zone for the tests
+      TimeZone.setDefault(TimeZone.getTimeZone("Europe/Dublin"));
+
+      //load the properties of the tests
+      testProps = new Properties();
+      URL properties = ExampleTaskTest.class.getClassLoader()
+        .getResource("test-config.properties");
+      testProps.load(properties.openStream());
+
+      //load the test configuration file
+      ConfigurationManager.loadConfig(null);
+
+      // Initialise the service manager kernel
+      kernelImpl = DSpaceKernelInit.getKernel(null);
+      if (!kernelImpl.isRunning())
+      {
+        kernelImpl.start(ConfigurationManager.getProperty("dspace.dir"));
+      }
+
+      // Applies/initializes our mock database by invoking its constructor
+      // (NOTE: This also initializes the DatabaseManager, which in turn
+      // calls DatabaseUtils to initialize the entire DB via Flyway)
+      new MockDatabaseManager();
+
+      // Initialize mock indexer (which does nothing, since Solr isn't running)
+      new MockIndexEventConsumer();
+
+      // Initialize mock Util class
+      new MockUtil();
+    } 
+    catch (IOException ex)
+    {
+      log.error("Error initializing tests", ex);
+      fail("Error initializing tests");
+    }
+  }
 
   @Before
   public void setUpStreams() {
@@ -52,7 +121,6 @@ public class ExampleTaskTest {
     final Context context = new Context();
     context.turnOffAuthorisationSystem();
 
-/*
     Group.initDefaultGroupNames(context);
 
     final EPerson eperson = EPerson.create(context);
@@ -66,7 +134,7 @@ public class ExampleTaskTest {
 
     // Set our global test EPerson as the current user in DSpace
     context.setCurrentUser(eperson);
-*/
+
     when(EPerson.findByEmail( any(Context.class), anyString() )).thenReturn(null);
 
     context.restoreAuthSystemState();
